8db6fd92dd20ad1bdc73525c913df761bf928137
wujiaxu <wujiaxu@sogou-inc.com>
2020-06-19 19:31:56 +0800

    add file

--- rust/0001_two-sum.rs
+++ rust/0001_two-sum.rs
@@ -2,9 +2,21 @@ impl Solution
 {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32>
     {
-        for i in &v
+        let mut res = Vec::new();
+        let n = nums.len();
+        'outer: for i in 0..n
         {
-            ;
+            for j in 0..i
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    res.push(j as i32);
+                    res.push(i as i32);
+                    break 'outer;
+                }
+            }
         }
+
+        return res;
     }
 }
